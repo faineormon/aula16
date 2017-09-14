25afce102ed634d950a8da95157488494df5804b
Aluno <Aluno>
2017-09-14 16:23:53 -0300

    atualização dos exercícios

--- ExerciciosJava/src/br/com/db1start/aula14/ExerciciosString11.java
+++ ExerciciosJava/src/br/com/db1start/aula14/ExerciciosString11.java
@@ -4,14 +4,14 @@ public class ExerciciosString11 {
 	
 	public Integer contandoLetras(String texto) {
 		String[] vogais = {"a","e","i","o","u"};
-		Integer i, j;
+		Integer i, j, quantVogais = 0;
 		
 		for(i = 0; i < texto.length(); i++) {
 			for(j = 0; j < vogais.length; j++)
-				if(vogais[j] == texto.substring(i,j)) {	
-				j++;
+				if(vogais[j].equals(texto.substring(i,i+1))) {	
+					quantVogais++;
 			}
 		}
-		return j;
+		return quantVogais;
 	}
 }
